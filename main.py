0f8c6f9f56f85df7ddb3349f3ff37c05d33f3e39
Anton <hunter.88@inbox.ru>
2023-03-15 11:30:36 +0500

    update

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import os
+from datetime import datetime
+import requests
+import json
+from bs4 import BeautifulSoup
+from fake_headers import Headers
 
 
 def logger(old_function):
 
     def new_function(*args, **kwargs):
-
+        answer = old_function(*args, **kwargs)
+        # print(old_function.__name__, answer)
+        with open('main.log', 'a') as f:
+            f.write(f'{datetime.now().strftime("%d.%m.%Y %H:%M:%S")} {old_function.__name__}({args}, {kwargs}) = {answer}\n')
+        return answer
     return new_function
 
 
@@ -48,10 +57,13 @@ def logger_p(path):
 
     def __logger(old_function):
         def new_function(*args, **kwargs):
-            ...
-
+            answer = old_function(*args, **kwargs)
+            nonlocal path
+            with open(path, 'a') as f:
+                f.write(
+                    f'{datetime.now().strftime("%d.%m.%Y %H:%M:%S")} {old_function.__name__}({args}, {kwargs}) = {answer}\n')
+            return answer
         return new_function
-
     return __logger
 
 
@@ -91,6 +103,41 @@ def test_2():
             assert str(item) in log_file_content, f'{item} должен быть записан в файл'
 
 
+def test_3():
+    HOST = "https://hh.ru/search/vacancy"
+    VACANCY = f"{HOST}?text=python&area=1&area=2"
+
+    def get_headers():
+        return Headers(browser="firefox", os="win").generate()
+
+    def get_text(url):
+        return requests.get(url, headers=get_headers()).text
+
+    @logger
+    def main():
+        result = {"vacancies": []}
+        html = get_text(f"{VACANCY}")
+        soup = BeautifulSoup(html, features="html5lib")
+        vacancies = soup.find_all(class_="serp-item")  # .find_all("article")
+        for item in vacancies:
+            a = item.find(class_='serp-item__title')
+            title = a.text
+            price = item.find(class_='bloko-header-section-3').text
+            if 'django' in title.lower() and 'flask' in title.lower() and 'usd' in price.lower():
+                result['vacancies'].append({
+                    "link": a['href'],
+                    "price": price,
+                    "company": item.find(class_='bloko-link bloko-link_kind-tertiary').text,
+                    "town": item.find(attrs={'data-qa': 'vacancy-serp__vacancy-address'}).text
+                })
+        return result
+
+    result = main()
+    with open('json_data.json', 'w') as f:
+        json.dump(result, f)
+
+
 if __name__ == '__main__':
     test_1()
     test_2()
+    test_3()
